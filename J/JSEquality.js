0a379a295abafe1fe5f4e17765b37f650b2eceec
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2023-12-31 14:34:49 +0530

    Update JSEquality.js

--- J/JSEquality.js
+++ J/JSEquality.js
@@ -63,3 +63,46 @@ async function k() {
   }
 }
 k();
+
+//Object.freeze() Shallow Freeze - Eg: When you are working with configuration  object that should not be changed, modified, added to or deleted from during runtime.
+const m = Object.freeze({
+  a: 1,
+  b: 2,
+  c: 3,
+});
+console.log(m);
+m.a = 4;
+console.log(m.a);//You cannot change or update the value of a property in object.freeze
+m.d = 4;
+console.log(m.d); //This returnes undefined because you cannot add any new property to object.freeze
+delete m.a;
+console.log(m.a); //You cannot delete inside object.freeze
+
+//Object.freeze() Deep Freeze - To enable deep freeze in a constant obejct that has key value pairs inside its key value pairs nested together you need a helper function that recursively freezes the object and each of its nested object. 
+const n = Object.freeze({
+  a: 1,
+  b:2,
+  c: 3,
+  d: 4,
+  e: { f: 5, g:6},
+  h: 7,
+});
+console.log(n);
+n.c = 30; //This will be ignored
+console.log(n.c);
+n.e.f = 50; //This will be executed and changed.
+console.log(n.e.f)
+
+//Object.seal() - Eg: Object.seal is used when you need to modify the existing data like in forms if you want to modify form field data but you dont want to delete or add any data.
+const o = Object.seal({
+  a:1,
+  b:2,
+  c:3,
+  d:4,
+})
+o.e = 5; //Addition of new key value pairs will be ignored
+console.log(o.e);
+delete o.a; //Deletion of existing key value pairs in Object.seal() will be ignored
+o.a = 10; 
+console.log(o.a); //  This will work because you can update the existing values in Object.seal()
+console.log(o.a);
