04d62f8644f74eaaadd6dc93d5a6e45795eeb66c
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-01-05 22:48:05 +0530

    Create IIFE.js

---
+++ Javascript/IIFE.js
@@ -0,0 +1,15 @@
+let paintColor = 'red'
+const paint = (() => {
+    return {
+        changeColorToBlue: () => {
+            paintcolor: 'Blue': return paintColor;
+        },
+        changeColorToGreen: () => {
+            paintColor: 'Green';
+            return paintColor;
+        }
+    }
+})();
+console.log(
+    paint.changeColorToBlue()
+);
