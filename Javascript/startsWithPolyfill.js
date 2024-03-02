cfbe1655127e6f316e60610e2ab6240e8f2f9728
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-03-02 23:00:41 +0530

    Create startsWithPolyfill.js

---
+++ Javascript/startsWithPolyfill.js
@@ -0,0 +1,7 @@
+// Polyfill for String.prototype.startsWith
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.substr(position, searchString.length) === searchString;
+  };
+}
