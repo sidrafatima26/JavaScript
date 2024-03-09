d45e39f7afe55dedb7c5bbfb81241165202c4e3c
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-03-09 18:26:19 +0530

    Update MapPolyfill.js

--- Javascript/MapPolyfill.js
+++ Javascript/MapPolyfill.js
@@ -1,4 +1,28 @@
+//Polyfill of map
+
+//arr.map((item, index, arr) => )
+Array.prototype.myMap = function (cb){
+  var arr = []
+  for(let i=0; i< this.length; i++)
+    arr.push(cb(this[i], i, this))
+  return arr;
+}
+
+const numbers = [1, 2, 3, 4, 5];
+
+let multiplyby2  = numbers.myMap( item => item*2 )
+console.log(multiplyby2)
+
+
+
 // Polyfill for Array.prototype.map
+/*The code checks if the Array.prototype.map method already exists. If it doesn't, it proceeds to define it.
+
+Inside the Array.prototype.map function, it first checks if this is null or not defined. If it is, it throws a TypeError.
+
+It then checks if the provided callback parameter is a function. If it's not, it throws a TypeError.
+
+It creates a new array (mappedArray) to store the results of applying the callback function to each element of the original array.*/
 if (!Array.prototype.map) {
   Array.prototype.map = function(callback, thisArg) {
     if (this == null) {
