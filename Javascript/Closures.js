a3f7d06b663bc5f2387c85f371a35401424586ff
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-03-10 22:48:30 +0530

    Update Closures.js

--- Javascript/Closures.js
+++ Javascript/Closures.js
@@ -1,5 +1,28 @@
 //Closures are functions bunduled to gether or enclsed along with references to its lexical environment. When an inner function has the scope of an outer function it is a closue
+//Write a function that will allow you to do this
 
+var addsix = createBase(6);
+addsix(10); //output: 16
+addsix(21); //output: 27
+
+function createBase(num1) {
+  return function (num2) {
+    console.log( num1 + num2);
+  }
+}
+
+//Use var only and print 0, 1,2 ..
+  for(var i=0;i< 3;i++){
+    function A(i){ 
+      setTimeout( function(){
+        console.log(i)
+      }, 1000 * i )
+    }
+    A(i);
+  }
+
+
+//Add and count
 function add() {
   let cnt = 0;
   function count() {
