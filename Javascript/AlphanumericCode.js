52c539b94a95ca2714a02dcfb38cd59599b8f2ef
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-03-18 23:45:51 +0530

    Create AlphanumericCode.js

---
+++ Javascript/AlphanumericCode.js
@@ -0,0 +1,14 @@
+function generateUniqueCode(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let code = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    code += characters[randomIndex];
+  }
+
+  return code;
+}
+
+const uniqueCode = generateUniqueCode(8); // Change 8 to the desired length of the unique code
+console.log(uniqueCode);
