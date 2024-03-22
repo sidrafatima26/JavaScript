f1b3d224e57bc500e77c3e7e997c544d0c4ab17e
Sidra Fatima <140838181+sidrafatima26@users.noreply.github.com>
2024-03-22 23:12:47 +0530

    Create Equality.js

---
+++ Javascript/Equality.js
@@ -0,0 +1,12 @@
+let x=0, y=1, z=0;
+if(x==y==z)
+  console.log("Enter")
+
+else
+  console.log("Exit")
+
+// In this code snippet, you're initializing three variables: x, y, and z, with values 0, 1, and 0 respectively. Then, you're using an if statement to compare if x is equal to y, and if that result is equal to z. Let's break down what happens:
+
+// x == y: This evaluates to false because x is 0 and y is 1.
+// false == z: Here, false (which resulted from the previous comparison) is compared to z, which is 0. In JavaScript, false is equal to 0, so this comparison is true.
+// So, the condition x == y == z evaluates to true.
